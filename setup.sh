d38523ffcc1cc9850e9b97f416b673021dd7d503
Helio Frota <00hf11@gmail.com>
2017-11-17 16:15:05 -0300

    bug fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # installs bash git prompt
-git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1 ~/
 
 # installs nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
