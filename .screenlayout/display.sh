99581d7628c6750c1eee0137a066b624f89e3194
Helio Frota <00hf11@gmail.com>
2024-07-22 12:13:59 -0300

    randr stuff

---
+++ .screenlayout/display.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-2 --primary --mode 1368x768 --pos 1600x0 --rotate normal --output DP-4 --off --output HDMI-2 --off --output HDMI-3 --off --output DP-5 --off --output HDMI-4 --off --output eDP-1-1 --off --output HDMI-1-1 --mode 1600x900 --pos 0x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
