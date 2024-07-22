91ca1b2168874c4de7de46ada83f0b41f94d54b7
Helio Frota <00hf11@gmail.com>
2024-07-22 13:16:40 -0300

    polybar and rofi

---
+++ .config/polybar/fix.sh
@@ -0,0 +1,5 @@
+polybar --list-monitors | while IFS=$'\n' read line; do
+  monitor=$(echo $line | cut -d':' -f1)
+  primary=$(echo $line | cut -d' ' -f3)
+  MONITOR=$monitor polybar --reload toph &
+done
