aa1a6ba62844a0c502afbef58f919491712e080f
Helio Frota <00hf11@gmail.com>
2024-07-26 14:13:36 -0300

    updates
    * enable mouse on tmux
    * fix neovim freeze thanks to
    https://github.com/LazyVim/LazyVim/issues/1581#issuecomment-1747106169

--- .config/nvim/lua/vimopts.lua
+++ .config/nvim/lua/vimopts.lua
@@ -21,3 +21,5 @@ vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+vim.opt.foldmethod = "manual"
