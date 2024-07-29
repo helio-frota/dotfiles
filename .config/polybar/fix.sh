e9fb2481f1f5227f2bdb4ffebb66b1665f64a160
Helio Frota <00hf11@gmail.com>
2024-07-28 21:49:07 -0300

    updates

--- .config/polybar/fix.sh
+++ .config/polybar/fix.sh
@@ -1,5 +1,5 @@
 polybar --list-monitors | while IFS=$'\n' read line; do
   monitor=$(echo $line | cut -d':' -f1)
   primary=$(echo $line | cut -d' ' -f3)
-  MONITOR=$monitor polybar --reload toph &
+  MONITOR=$monitor polybar --reload example &
 done
