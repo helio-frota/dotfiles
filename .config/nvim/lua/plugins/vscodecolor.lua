7bad15ac8336d910459116a6c1dc4d1977ae4bfa
Helio Frota <00hf11@gmail.com>
2024-08-12 15:31:06 -0300

    rice-ing

--- .config/nvim/lua/plugins/vscodecolor.lua
+++ .config/nvim/lua/plugins/vscodecolor.lua
@@ -1,6 +1,13 @@
 return {
   "Mofiqul/vscode.nvim",
   config = function()
+    require("vscode").setup {
+      style = "light",
+      transparent = true,
+      italic_comments = true,
+      underline_links = true,
+      disable_nvimtree_bg = true,
+    }
     require("vscode").load "light"
   end,
 }
