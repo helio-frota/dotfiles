0bfc32284480deec2ea3f5da2da89483c6668d14
Helio Frota <00hf11@gmail.com>
2024-10-03 20:52:57 -0300

    auto save...

---
+++ .config/nvim/lua/plugins/autosave.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle",
+    event = { "InsertLeave", "TextChanged" },
+    opts = {
+      enabled = true,
+      execution_message = {
+        enabled = true,
+        message = function()
+          return ("Saved at " .. vim.fn.strftime "%H:%M:%S")
+        end,
+        cleaning_interval = 1250,
+      },
+      trigger_events = {
+        immediate_save = { "BufLeave", "FocusLost" },
+        defer_save = { "InsertLeave", "TextChanged" },
+        cancel_defered_save = { "InsertEnter" },
+      },
+      condition = nil,
+      write_all_buffers = false,
+      noautocmd = false,
+      lockmarks = false,
+      debounce_delay = 1000,
+      debug = false,
+    },
+  },
+}
