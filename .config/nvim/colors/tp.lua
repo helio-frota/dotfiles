271dd946d848eab77475f11e083b164f31e15423
Helio Frota <00hf11@gmail.com>
2024-11-23 11:40:23 -0300

    I'm not using debugger for rust (println! ftw)

--- .config/nvim/colors/tp.lua
+++ .config/nvim/colors/tp.lua
@@ -1,6 +1,6 @@
 vim.cmd [[highlight clear]]
 
-local highlight = function(group, bg, fg, attr)
+local hl = function(group, bg, fg, attr)
   fg = fg and "guifg=" .. fg or ""
   bg = bg and "guibg=" .. bg or ""
   attr = attr and "gui=" .. attr or ""
@@ -8,6 +8,7 @@ local highlight = function(group, bg, fg, attr)
 end
 
 local comments = "#008000"
+local info = "#008000"
 local selected_text = "#0078d7"
 local chars_and_strings = "#008080"
 local numbers = "#AD4011"
@@ -27,58 +28,72 @@ local title = "#99b4d1"
 local string_escape = "#465a7d"
 local background = "#fefefe"
 
-highlight("Character", nil, chars_and_strings, nil)
-highlight("IncSearch", match_search, nil, nil)
-highlight("Comment", nil, comments, "italic")
-highlight("CursorLine", cursor_line, nil, nil)
-highlight("String", nil, chars_and_strings, nil)
-highlight("StringEscape", nil, string_escape, nil)
-highlight("Function", nil, keywords2, nil)
-highlight("FunctionBuiltin", nil, keywords2, nil)
-highlight("FunctionMacro", nil, keywords2, nil)
-highlight("PunctBracket", nil, brackets, nil)
-highlight("Keyword", nil, keywords1, nil)
-highlight("Keyword2", nil, keywords2, nil)
-highlight("LineNr", nil, line_number, nil)
-highlight("Number", nil, numbers, nil)
-highlight("Normal", background, text, nil)
-highlight("Visual", selected_text, background, nil)
+hl("Character", nil, chars_and_strings, nil)
+hl("IncSearch", match_search, nil, nil)
+hl("Comment", nil, comments, "italic")
+hl("CursorLine", cursor_line, nil, nil)
+hl("String", nil, chars_and_strings, nil)
+hl("StringEscape", nil, string_escape, nil)
+hl("Function", nil, keywords2, nil)
+hl("FunctionBuiltin", nil, keywords2, nil)
+hl("FunctionMacro", nil, keywords2, nil)
+hl("PunctBracket", nil, brackets, nil)
+hl("Keyword", nil, keywords1, nil)
+hl("Keyword2", nil, keywords2, nil)
+hl("LineNr", nil, line_number, nil)
+hl("Number", nil, numbers, nil)
+hl("Normal", background, text, nil)
+hl("Visual", selected_text, background, nil)
 
 -- telescope
-highlight("TelescopeNormal", background, nil, nil)
-highlight("TelescopeMatching", match_search, nil, nil)
-highlight("TelescopePreviewLine", match_search, nil, nil)
-highlight("TelescopeTitle", nil, title, nil)
-highlight("TelescopeSelection", selected_text, background, nil)
-highlight("TelescopeBorder", nil, border, nil)
+hl("TelescopeNormal", background, nil, nil)
+hl("TelescopeMatching", match_search, nil, nil)
+hl("TelescopePreviewLine", match_search, nil, nil)
+hl("TelescopeTitle", nil, title, nil)
+hl("TelescopeSelection", selected_text, background, nil)
+hl("TelescopeBorder", nil, border, nil)
 
 -- barbar
-highlight("BufferCurrent", background, nil, nil)
-highlight("BufferCurrentMod", background, nil, nil)
-highlight("BufferCurrentIndex", background, nil, nil)
-highlight("BufferCurrentSign", background, nil, nil)
-highlight("BufferInactive", background, nil, nil)
-highlight("BufferInactiveMod", background, nil, nil)
-highlight("BufferInactiveIndex", background, nil, nil)
-highlight("BufferInactiveSign", background, nil, nil)
-highlight("BufferVisible", background, nil, nil)
-highlight("BufferVisibleMod", background, nil, nil)
-highlight("BufferVisibleIndex", background, nil, nil)
-highlight("BufferVisibleSign", background, nil, nil)
-highlight("BufferAlternate", background, nil, nil)
-highlight("BufferError", error, nil, nil)
-highlight("BufferWarning", warning, nil, nil)
-highlight("BufferTabpageFill", background, nil, nil)
-highlight("BufferTabpages", background, nil, nil)
+hl("BufferCurrent", background, nil, nil)
+hl("BufferCurrentMod", background, nil, nil)
+hl("BufferCurrentIndex", background, nil, nil)
+hl("BufferCurrentSign", background, nil, nil)
+hl("BufferInactive", background, nil, nil)
+hl("BufferInactiveMod", background, nil, nil)
+hl("BufferInactiveIndex", background, nil, nil)
+hl("BufferInactiveSign", background, nil, nil)
+hl("BufferVisible", background, nil, nil)
+hl("BufferVisibleMod", background, nil, nil)
+hl("BufferVisibleIndex", background, nil, nil)
+hl("BufferVisibleSign", background, nil, nil)
+hl("BufferAlternate", background, nil, nil)
+hl("BufferError", error, nil, nil)
+hl("BufferWarning", warning, nil, nil)
+hl("BufferTabpageFill", background, nil, nil)
+hl("BufferTabpages", background, nil, nil)
 
 -- nvimtree
-highlight("NvimTreeNormal", nil, text, nil)
-highlight("NvimTreeFolderName", nil, folder_name, nil)
-highlight("NvimTreeFolderIcon", nil, folder, nil)
-highlight("NvimTreeOpenedFolderName", nil, folder_name, nil)
-highlight("NvimTreeFileName", nil, text, nil)
-highlight("NvimTreeWinSeparator", nil, border, nil)
+hl("NvimTreeNormal", nil, text, nil)
+hl("NvimTreeFolderName", nil, folder_name, nil)
+hl("NvimTreeFolderIcon", nil, folder, nil)
+hl("NvimTreeOpenedFolderName", nil, folder_name, nil)
+hl("NvimTreeFileName", nil, text, nil)
+hl("NvimTreeWinSeparator", nil, border, nil)
 
+-- nvim-notify
+hl("NotifyINFOBorder", nil, border, nil)
+hl("NotifyWARNBorder", nil, border, nil)
+hl("NotifyERRORBorder", nil, border, nil)
+hl("NotifyDEBUGBorder", nil, border, nil)
+hl("NotifyTRACEBorder", nil, border, nil)
+hl("NotifyINFOTitle", nil, info, nil)
+hl("NotifyINFOIcon", nil, info, nil)
+hl("NotifyERRORTitle", nil, error, nil)
+hl("NotifyERRORIcon", nil, error, nil)
+hl("NotifyWARNTitle", nil, warning, nil)
+hl("NotifyWARNIcon", nil, warning, nil)
+
+-- treesitter
 vim.api.nvim_set_hl(0, "@character", { link = "Character" })
 vim.api.nvim_set_hl(0, "@number", { link = "Number" })
 vim.api.nvim_set_hl(0, "@comment", { link = "Comment" })
@@ -94,3 +109,10 @@ vim.api.nvim_set_hl(0, "@type", { link = "Keyword2" })
 vim.api.nvim_set_hl(0, "@type.builtin", { link = "Keyword2" })
 vim.api.nvim_set_hl(0, "@attribute", { link = "Keyword" })
 vim.api.nvim_set_hl(0, "@variable.builtin", { link = "Keyword" })
+
+-- noice
+vim.api.nvim_set_hl(0, "NoiceCmdlinePopupBorder", { fg = "#465a7d", bg = "NONE" })
+
+-- NoiceLspProgressClient: Nome do cliente LSP durante progresso.
+-- NoiceLspProgressSpinner: Spinner de progresso do LSP.
+-- NoiceLspProgressTitle: Título do progresso do LSP.
--- .config/nvim/lua/plugins/lsp-conf.lua
+++ .config/nvim/lua/plugins/lsp-conf.lua
@@ -24,31 +24,31 @@ return {
       }
     end,
   },
-  {
-    "mfussenegger/nvim-dap",
-    config = function()
-      local dap, dapui = require "dap", require "dapui"
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = function()
-      require("dapui").setup()
-    end,
-  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   config = function()
+  --     local dap, dapui = require "dap", require "dapui"
+  --     dap.listeners.before.attach.dapui_config = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.launch.dapui_config = function()
+  --       dapui.open()
+  --     end
+  --     dap.listeners.before.event_terminated.dapui_config = function()
+  --       dapui.close()
+  --     end
+  --     dap.listeners.before.event_exited.dapui_config = function()
+  --       dapui.close()
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "rcarriga/nvim-dap-ui",
+  --   dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+  --   config = function()
+  --     require("dapui").setup()
+  --   end,
+  -- },
   {
     "mrcjkb/rustaceanvim",
     version = "^5",
@@ -57,18 +57,18 @@ return {
     config = function(_, opts)
       vim.g.rustaceanvim = vim.tbl_deep_extend("force", {}, opts or {})
       -- dbg config
-      local mason_registry = require "mason-registry"
-      local codelldb = mason_registry.get_package "codelldb"
-      local extension_path = codelldb:get_install_path() .. "/extension/"
-      local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-      local cfg = require "rustaceanvim.config"
+      -- local mason_registry = require "mason-registry"
+      -- local codelldb = mason_registry.get_package "codelldb"
+      -- local extension_path = codelldb:get_install_path() .. "/extension/"
+      -- local codelldb_path = extension_path .. "adapter/codelldb"
+      -- local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+      -- local cfg = require "rustaceanvim.config"
 
-      vim.g.rustaceanvim = {
-        dap = {
-          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-        },
-      }
+      -- vim.g.rustaceanvim = {
+      --   dap = {
+      --     adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+      --   },
+      -- }
       -- dbg config
     end,
     opts = {
@@ -86,7 +86,7 @@ return {
           vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder)
           vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition)
           vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename)
-          vim.keymap.set({ "n", "v" }, "<space>do", vim.lsp.buf.code_action)
+          vim.keymap.set({ "n", "v" }, "<space>ct", vim.lsp.buf.code_action)
           vim.keymap.set("n", "gr", vim.lsp.buf.references)
           vim.keymap.set("n", "<space>f", function()
             vim.lsp.buf.format { async = true }
